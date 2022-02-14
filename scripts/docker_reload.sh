f203643ae6641dda987e6987ab5d728a7c4935df
srdn <srdn2417@gmail.com>
2022-02-14 12:46:51 +0200

    update reload script

--- scripts/docker_reload.sh
+++ scripts/docker_reload.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+# Bash instead of sh because source exists only in bash
 
 docker-compose down
 ./scripts/decode.sh
+chmod 755 ./open-store.sh
 ./open-store.sh
+source ./open-store.sh
 docker-compose -f docker-compose.yml up -d
